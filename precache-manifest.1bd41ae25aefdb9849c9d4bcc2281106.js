fe95bf22495b716c25cad2afc9508ac53a9e7d10
Malfit <oleksandr.rybakov@nure.ua>
2020-04-15 21:19:37 +0300

    Updates

---
+++ precache-manifest.1bd41ae25aefdb9849c9d4bcc2281106.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd52e43ecdf0c6d91363fbae3e3c6938",
+    "url": "/Cursor-React-Homework3/index.html"
+  },
+  {
+    "revision": "50b9c96f011b4e953162",
+    "url": "/Cursor-React-Homework3/static/css/main.f0853224.chunk.css"
+  },
+  {
+    "revision": "f31212f4b85eaa8993fa",
+    "url": "/Cursor-React-Homework3/static/js/2.53995ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cursor-React-Homework3/static/js/2.53995ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50b9c96f011b4e953162",
+    "url": "/Cursor-React-Homework3/static/js/main.1548e070.chunk.js"
+  },
+  {
+    "revision": "747f046b8c228199b64e",
+    "url": "/Cursor-React-Homework3/static/js/runtime-main.5f9f3d7a.js"
+  },
+  {
+    "revision": "3e669a28ab8639bb7e7d41c370610448",
+    "url": "/Cursor-React-Homework3/static/media/123.3e669a28.jpg"
+  },
+  {
+    "revision": "86fd39290dbe58a9fabd95be6508731a",
+    "url": "/Cursor-React-Homework3/static/media/234.86fd3929.jpg"
+  },
+  {
+    "revision": "47cc7fcfc289bbaf3b5197f0a3d78d14",
+    "url": "/Cursor-React-Homework3/static/media/board_black_line_texture_background_wood_55220_1280x720.47cc7fcf.jpg"
+  }
+]);
